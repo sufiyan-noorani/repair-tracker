266545a651f461d810bd73174f797a2b4bc239e0
Sufiyan Noorani <noorani.sufiyan@gmail.com>
2026-04-23 14:43:54 +0530

    Orders App initial setup, added gitignore

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Order(models.Model):
+    customer_name = models.CharField(max_length=100)
+    device_name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.customer_name
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("This is home page")
+
+def order_list(request):
+    return HttpResponse("This is the orders page.")
