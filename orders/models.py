6513af15c0846684d9ee3ec264262cf9c1290f0f
Sufiyan Noorani <noorani.sufiyan@gmail.com>
2026-04-23 18:40:05 +0530

    Fixed __str__ method to ensure a string return

--- orders/models.py
+++ orders/models.py
@@ -7,4 +7,4 @@ class Order(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.customer_name
+        return str(self.customer_name)
