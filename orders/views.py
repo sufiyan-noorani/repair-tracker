db036532ec16bef40b81415004577e06f4f5198b
Sufiyan Noorani <noorani.sufiyan@gmail.com>
2026-04-28 16:33:31 +0530

    Added order list view and template to display stored orders

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Order
 
 def home(request):
     return HttpResponse("This is home page")
 
 def order_list(request):
-    return HttpResponse("This is the orders page.")
+    orders = Order.objects.all() # pylint: disable=no-member
+    return render(request, 'orders/orders_list.html', {"orders": orders})
